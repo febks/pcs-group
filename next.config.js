6350b180a7c73d3e37d89386d8f91fb8b307a88e
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-14 01:12:44 +0700

    fix distdir

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
   webpack: (config, { nextRuntime }) => {
     if (typeof nextRuntime === "undefined") {
