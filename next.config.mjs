7d9fe29d90fdd7702995e48e2d073d801bab0c00
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-14 01:03:15 +0700

    add webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  reactStrictMode: true,
+  webpack: (config, { nextRuntime }) => {
+    if (typeof nextRuntime === "undefined") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
